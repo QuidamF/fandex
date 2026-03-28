bf7b807f48c92310b63c2d1182b46c0004600c6b
Edgar Lopez Diaz <edgarldqf@gmail.com>
2026-03-28 02:54:05 -0600

    fix: add interpreter none to ecosystem config to prevent node.js syntax error

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script: "./venv/bin/gunicorn",
     args: "-w 4 -b 0.0.0.0:5000 main:app",
     cwd: ".",
+    interpreter: "none",
     env: {
       FLASK_ENV: "production",
       PORT: 5000
